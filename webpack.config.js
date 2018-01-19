6a280db49f2e0a2f855505741c69c943645b3e5e
Kim Il Sik <kimilsik@Kims-MacBook.local>
2018-01-19 23:49:15 +0900

    heroku extract text webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -56,38 +56,3 @@ module.exports = (env) => {
         }
     }
 };
-
-//
-// // path should be absolute.
-// module.exports = {
-//     entry : './src/app.js',
-//     output : {
-//         path : path.resolve(__dirname, 'public'),
-//         filename : 'bundle.js'
-//     },
-//     module : {
-//         rules : [
-//             {
-//                 loader : 'babel-loader',
-//                 test : /.js$/,
-//                 exclude : /node_modules/
-//             },
-//             {
-//                 // test : /.(css|scss)$/,
-//                 test : /\.s?css$/,
-//                 use : [
-//                     'style-loader',
-//                     'css-loader',
-//                     'sass-loader'
-//                 ]
-//             }
-//         ]
-//     },
-//     devtool : 'cheap-module-eval-source-map',
-//     devServer : {
-//         contentBase : path.resolve(__dirname, 'public'),
-//         historyApiFallback : true
-//     }
-// };
-
-// loader
