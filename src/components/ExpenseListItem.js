8c2a8c36a0a25a3fb9f27cc6b2724febb156ae3c
Kim Il Sik <kimilsik@Kims-MacBook.local>
2018-01-20 22:51:12 +0900

    setup formatting for amount and createdAt

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -4,6 +4,8 @@
 
 import React from 'react';
 import {Link} from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 export const ExpenseListItem = //(props) => (
     ({id, description, amount, createdAt}) => (
@@ -11,7 +13,10 @@ export const ExpenseListItem = //(props) => (
         <Link to={`/edit/${id}`}>
             <h3>{description}</h3>
         </Link>
-        <p>amount : {amount}, createdAt : {createdAt}</p>
+        <p>
+            amount : {numeral(amount / 100).format('$0.0.00')},
+            createdAt : {moment(createdAt).format('MMMM Do, YYYY')}
+            </p>
         {/*actually remove it.*/}
     </div>
 );
--- src/tests/components/ExpenseListItem.test.js
+++ src/tests/components/ExpenseListItem.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { ExpenseListItem } from '../../components/ExpenseListItem';
 import expenses from '../fixtures/expenses';
+import moment from 'moment';
+import numeral from 'numeral';
 
 // create test file
 // grap imports
@@ -11,5 +13,5 @@ import expenses from '../fixtures/expenses';
 test('should render ExpenseListItem correctly', () => {
     const wrapper = shallow(<ExpenseListItem {...expenses[0]}/>);
     expect(wrapper).toMatchSnapshot();
-    expect(wrapper.find('div').find('p').text()).toBe(`amount : ${expenses[0].amount}, createdAt : ${expenses[0].createdAt}`);
+    expect(wrapper.find('div').find('p').text()).toBe(`amount : ${numeral(expenses[0].amount / 100).format('$0.0.00')}, createdAt : ${moment(expenses[0].createdAt).format('MMMM Do, YYYY')}`);
 });
