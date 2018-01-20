5bc7a34dc27d9bdd2c53d4428d348e0ea0ba2a35
Kim Il Sik <kimilsik@Kims-MacBook.local>
2018-01-20 22:23:21 +0900

    dev dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -7,11 +7,10 @@ module.exports = (env) => {
     const isProduction = env === 'production';
     const CSSExtract = new ExtractTextPlugin('styles.css');
 
-    // console.log(env);
     return {
         entry : './src/app.js',
         output : {
-            path : path.resolve(__dirname, 'public'),
+            path : path.resolve(__dirname, 'public', 'dist'),
             filename : 'bundle.js'
         },
         module : {
@@ -52,7 +51,8 @@ module.exports = (env) => {
         devtool : isProduction ? 'source-map' : 'inline-source-map',
         devServer : {
             contentBase : path.resolve(__dirname, 'public'),
-            historyApiFallback : true
+            historyApiFallback : true,
+            publicPath : '/dist/'
         }
     }
 };
